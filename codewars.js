d5f044a9d053110cfe132ac40a4f8a6c3d325e19
MeganMFisher <meganmichelle318@gmail.com>
2017-04-24 20:57:55 -0600

    rock paper scissors

--- codewars.js
+++ codewars.js
@@ -77,5 +77,85 @@ function find_average(array) {
   return array.reduce((a, b) => (a + b)) / array.length;
 }
 
-// QUESTION 
+// QUESTION Number of People in the Bus
 
+var number = function(busStops){
+  var peeps = 0;
+  for(var i = 0; i < busStops.length; i++) {
+   peeps += busStops[i][0] 
+   peeps -= busStops[i][1]
+  }
+  return peeps
+}
+
+// other solutions
+
+const number = (busStops) => busStops.reduce((rem, [on, off]) => rem + on - off, 0);
+
+var number = function(busStops){
+  return busStops.map(x => x[0] - x[1]).reduce( (x, y) => x + y);
+}
+
+const number = busStops => busStops.reduce((n, [on, off]) => n + on - off, 0);
+
+// QUESTION Rock Paper Scissors
+
+const rps = (p1, p2) => {
+ const getMsg = (n) => `Player ${n} won!`;
+
+  if(p1 === 'rock' && p2 === 'scissors') {
+      return getMsg(1)
+  } else if(p1 === 'scissors' && p2 === 'paper') {
+      return getMsg(1)
+  } else if(p1 === 'paper' && p2 === 'rock') {
+      return getMsg(1)
+  } else if(p2 === 'rock' && p1 === 'scissors') {
+      return getMsg(2)
+  } else if(p2 === 'scissors' && p1 === 'paper') {
+      return getMsg(2)
+  } else if(p2 === 'paper' && p1 === 'rock') {
+      return getMsg(2)
+  } else if(p1 === 'rock' && p2 === 'rock') {
+      return 'Draw!'
+  } else if(p2 === 'scissors' && p1 === 'scissors') {
+      return 'Draw!'
+  } else if(p2 === 'paper' && p1 === 'paper') {
+      return 'Draw!'
+    }
+};
+
+// other solutions
+
+const rps = (p1, p2) => {
+  if(p1 === p2) {
+    return 'Draw!'
+  }; 
+  return `Player ${/rockscissors|scissorspaper|paperrock/.test(p1+p2)? 1 : 2} won!`;
+}
+
+const rps = (p1, p2) => {
+  if (p1 === p2) return "Draw!";
+  var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+  if (p2 === rules[p1]) {
+    return "Player 1 won!";
+  }
+  else {
+    return "Player 2 won!";
+  }
+};
+
+const rps = (p1, p2) => {
+  if (p1 == p2)
+    return 'Draw!';
+    
+   if (p1 == 'rock' && p2 == 'scissors') 
+     return 'Player 1 won!'
+   else if (p1 == 'scissors' && p2 == 'paper') 
+     return 'Player 1 won!'
+   else if (p1 == 'paper' && p2 == 'rock') 
+     return 'Player 1 won!'
+   else
+     return 'Player 2 won!';
+};
+
+// QUESTION 
