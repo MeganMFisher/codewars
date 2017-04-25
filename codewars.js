f53cc6fee150239ea00c77a531028c58c1e6cdf1
MeganMFisher <meganmichelle318@gmail.com>
2017-04-24 19:45:24 -0600

    initial commit

---
+++ codewars.js
@@ -0,0 +1,15 @@
+function boolToWord( bool ){
+  return bool ? 'Yes':'No';
+}
+
+function boolToWord( bool ){
+  if (bool) {
+    return 'Yes';
+  } else {
+    return 'No';
+  }
+}
+
+let boolToWord = bool => bool ? 'Yes' : 'No';
+
+// -----------------------------
