af83bffbbb365042d6c3fff82f5ae997eefab346
MeganMFisher <meganmichelle318@gmail.com>
2017-04-24 20:19:59 -0600

    find average

--- codewars.js
+++ codewars.js
@@ -1,3 +1,15 @@
+//If true return 'Yes' if false return 'No'
+
+function boolToWord(bool) {
+  if(bool === true) {
+    return 'Yes';
+  } else {
+    return 'No';
+  }
+}
+
+//Other answers
+
 function boolToWord( bool ){
   return bool ? 'Yes':'No';
 }
@@ -12,4 +24,52 @@ function boolToWord( bool ){
 
 let boolToWord = bool => bool ? 'Yes' : 'No';
 
-// -----------------------------
+// QUESTION: Invert numbers in array
+
+function invert(array) {
+var arr = [];
+  for(var i = 0; i < array.length; i++) {
+    if(array[i] !== 0) {
+    arr.push(array[i] * -1)
+    } else {
+      arr.push(array[i])
+    }
+   }
+   return arr
+}
+
+// other solutions
+
+function invert(array) {
+   return array.map( x => x === 0 ? x : -x);
+}
+
+function invert(array) {
+   return array.map(n => n && -n)
+}
+
+function invert(array) {
+   return array.map(v => ~v+1);
+}
+
+// QUESTION find average of number in array
+
+
+
+// other solutions
+
+function find_average(array) {
+  var sum = array.reduce((a, b) => a + b, 0);
+  return sum/array.length;
+}
+
+function find_average(array) {
+  return array.reduce((result, current) => result + current, 0) / array.length;
+}
+
+function find_average(array) {
+  return array.reduce((a, b) => (a + b)) / array.length;
+}
+
+// QUESTION 
+
