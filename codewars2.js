71682157b2b9d521f526d203224fd40b7a363283
MeganMFisher <meganmichelle318@gmail.com>
2017-05-01 19:52:48 -0600

    youre a square

--- codewars2.js
+++ codewars2.js
@@ -12,5 +12,22 @@ function disemvowel(str) {
 
 disemvowel = str => str.replace(/[aeiou]/gi,'');
 
-// QUESTION 
+// QUESTION you're a square
 
+var isSquare = function(n){
+  return n >= 0 && Math.sqrt(n) % 1 === 0;
+}
+
+// other answers
+
+function isSquare(n) {
+  return Math.sqrt(n) % 1 === 0;
+}
+
+const isSquare = n => Number.isInteger(Math.sqrt(n));
+
+var isSquare = function(n){
+  return Number.isInteger(Math.sqrt(n));
+}
+
+// QUESTION
