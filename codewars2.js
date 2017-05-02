11eca316b3d1aa4d42a25840dbf302d83a14642b
MeganMFisher <meganmichelle318@gmail.com>
2017-05-02 13:36:56 -0600

    direction reduction

--- codewars2.js
+++ codewars2.js
@@ -264,6 +264,65 @@ function toCamelCase(str){
 }
 
 
+// QUESTION direction reduction
+
+dirReduc = (arr) => {
+  var i = 0;    
+  while(i < arr.length) {
+    if((arr[i] === "NORTH" && arr[i+1] === "SOUTH") ||
+    (arr[i] === "SOUTH" && arr[i+1] === "NORTH") ||
+    (arr[i] === "EAST" && arr[i+1] === "WEST") ||
+    (arr[i] === "WEST" && arr[i+1] === "EAST")) {
+        arr.splice(i, 2);
+        i--;
+    } else {
+      i++;
+    }
+  }
+  return arr;
+}
+
+// other answers
+
+function dirReduc(plan) {
+  var opposite = {
+    'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'};
+  return plan.reduce(function(dirs, dir){
+      if (dirs[dirs.length - 1] === opposite[dir])
+        dirs.pop();
+      else
+        dirs.push(dir);
+      return dirs;
+    }, []);
+}
+
+function dirReduc(arr) {
+  var str = arr.join(''), pattern = /NORTHSOUTH|EASTWEST|SOUTHNORTH|WESTEAST/;
+  while (pattern.test(str)) str = str.replace(pattern,'');
+  return str.match(/(NORTH|SOUTH|EAST|WEST)/g)||[];
+}
+
+function isOppo(dir1,dir2) {
+    if (dir1 + dir2 === 'SOUTHNORTH') return true;
+    if (dir1 + dir2 === 'NORTHSOUTH') return true;
+    if (dir1 + dir2 === 'EASTWEST') return true;
+    if (dir1 + dir2 === 'WESTEAST') return true;
+    return false;
+}
+  
+function dirReduc(arr){
+  var len = arr.length
+  for (var i = 0; i < len - 1; i++) {
+    if (isOppo(arr[i], arr[i+1])) {
+      arr.splice(i,2);
+      return dirReduc(arr);
+    }
+  }
+  return arr;
+}
+
+// QUESTION 
+
 
 
 
