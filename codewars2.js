c868e7ed733a92d57011fcd8c03239ea68cff557
MeganMFisher <meganmichelle318@gmail.com>
2017-05-02 12:05:11 -0600

    simple pit latin

--- codewars2.js
+++ codewars2.js
@@ -210,7 +210,33 @@ function duplicateEncode(word) {
   }).join('')
 }
 
-// QUESTION
+// QUESTION Simple Pig Latin
+
+function pigIt(str){
+  return str.replace(/\b(\w)(\w+)/g,function(a,b,c){return c+b+"ay";});
+}
+
+// other answers
+
+function pigIt(str){
+  return str.split(' ').map(function(el){
+    return el.slice(1) + el.slice(0,1) + 'ay';
+  }).join(' ');
+}
+
+function pigIt(str){
+  return str.replace(/\b(\w)(\w+)\b/ig,"$2$1ay");
+}
+
+function pigIt(str){
+  var words = str.split(" ");
+  for (w in words) {
+    words[w] = words[w].slice(1) + words[w].slice(0,1) + "ay"
+  }
+  return words.join(" ");
+}
+
+// QUESTION 
 
 
 
