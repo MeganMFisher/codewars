46f59cc1d9ddb04631a4c7dc67918a3b164f53b1
MeganMFisher <meganmichelle318@gmail.com>
2017-05-02 19:45:58 -0600

    your order, please

--- codewars2.js
+++ codewars2.js
@@ -321,7 +321,39 @@ function dirReduc(arr){
   return arr;
 }
 
-// QUESTION 
+// QUESTION Your order, please
+
+order = (words) => {
+  return words.split(' ').sort(( a, b ) => {
+      var num1 = +(a.match(/\d+/g)[0]);
+      var num2 = +(b.match(/\d+/g)[0]);
+      return num1 - num2;
+    }).join(' ');
+}
+
+// other answers
+
+function order(words){
+  
+  return words.split(' ').sort(function(a, b){
+      return a.match(/\d/) - b.match(/\d/);
+   }).join(' ');
+}  
+
+function order(words){
+  var array = words.split(' ');
+  var sortedArray = [];
+  for(i = 0; i <= array.length; i++) {
+    for(j = 0; j < array.length; j++) {
+      if(array[j].indexOf(i) >= 0) {
+        sortedArray.push(array[j]);
+      }
+    }
+  }
+  return sortedArray.join(' ');
+}
+
+// QUESTION
 
 
 
