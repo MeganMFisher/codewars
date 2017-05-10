3fdda66207b01937289cec524fa8cc19fa401997
MeganMFisher <meganmichelle318@gmail.com>
2017-05-09 21:54:14 -0600

    where my anagram at

--- codewars3.js
+++ codewars3.js
@@ -12,3 +12,26 @@ function solution(string) {
   return(string.replace(/([A-Z])/g, ' $1'));
 
 }
+
+
+// QUESTION Where's my anagrams at?
+
+anagrams = (word, words) => {
+
+  return words.filter(function(e){
+    var anagrams = e.split('').sort().join('') 
+    var testWord = word.split('').sort().join('')
+    return anagrams === testWord
+  })
+
+
+}
+
+// other answers 
+
+function anagrams(word, words) {
+  word = word.split('').sort().join('');
+  return words.filter(function(v) {return word == v.split('').sort().join('');});
+}
+
+let anagrams = (word, words) => words.filter(w => w.split('').sort().join('') === word.split('').sort().join(''));
